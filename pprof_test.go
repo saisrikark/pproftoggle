d5936ebf41e4c3700be2a150648c5fae418e31fe
saisrikark <saisrikark@gmail.com>
2023-07-14 20:23:01 +0530

    more tests

--- pprof_test.go
+++ pprof_test.go
@@ -13,8 +13,10 @@ import (
 )
 
 var (
-	TestingPort = "3125"
-	BASE_URL    = "http://localhost:" + TestingPort + "/debug/pprof"
+	PORT      = "3125"
+	LOCALHOST = "127.0.0.1"
+	BASE_URL  = "http://" + LOCALHOST + ":" + PORT + "/debug/pprof"
+	ENDDPOINT = "/"
 )
 
 func TestListen(t *testing.T) {
@@ -34,9 +36,9 @@ func TestListen(t *testing.T) {
 
 		wg.Add(1)
 		go func() {
-			lc, err = net.Listen("tcp", ":"+TestingPort)
+			lc, err = net.Listen("tcp", ":"+PORT)
 			if err != nil {
-				t.Errorf("unable to create listner at port:[%s] err:[%s]", TestingPort, err.Error())
+				t.Errorf("unable to create listner at port:[%s] err:[%s]", PORT, err.Error())
 			}
 			wg.Done()
 		}()
@@ -46,7 +48,7 @@ func TestListen(t *testing.T) {
 		}
 
 		ppfs, err := pproftoggle.NewServer(pproftoggle.ServerConfig{
-			HttpServer: &http.Server{Addr: ":" + TestingPort},
+			HttpServer: &http.Server{Addr: ":" + PORT},
 		})
 		if err != nil {
 			t.Errorf("unable to initialise new server")
@@ -55,7 +57,7 @@ func TestListen(t *testing.T) {
 		ctx, _ := context.WithTimeout(context.Background(), time.Millisecond)
 		err = ppfs.Listen(ctx)
 		if err == nil {
-			t.Errorf("serving requests at port should not possible as port [%s] is blocked", TestingPort)
+			t.Errorf("serving requests at port should not possible as port [%s] is blocked", PORT)
 		} else {
 			t.Logf("as expected encountered error:[%s] while trying to server request on a blocked port", err.Error())
 		}
@@ -65,7 +67,7 @@ func TestListen(t *testing.T) {
 		ctx, cancel := context.WithCancel(context.Background())
 
 		ppfs, err := pproftoggle.NewServer(pproftoggle.ServerConfig{
-			HttpServer: &http.Server{Addr: ":" + TestingPort},
+			HttpServer: &http.Server{Addr: ":" + PORT},
 		})
 		if err != nil {
 			t.Errorf("unable to initialise new server")
@@ -78,14 +80,17 @@ func TestListen(t *testing.T) {
 		}()
 
 		for i := 0; i < 5; i++ {
-			time.Sleep(time.Millisecond * 10)
-			if ppfs.IsRunning() {
+			if isPortUsed(PORT) {
 				break
 			}
+			time.Sleep(time.Millisecond * 10)
+		}
+		if !isPortUsed(PORT) {
+			t.Errorf("port:[%s] not being used", PORT)
 		}
 
 		resp := ""
-		if err := requests.URL("/").BaseURL(BASE_URL).ToString(&resp).Fetch(context.Background()); err != nil {
+		if err := requests.URL(ENDDPOINT).BaseURL(BASE_URL).ToString(&resp).Fetch(context.Background()); err != nil {
 			t.Errorf("unable to fetch from /trace endpoint err:[%s]", err)
 		} else {
 			t.Logf("received a respose from the endpoint truncated resp\n[%s]", resp[0:30])
@@ -96,5 +101,109 @@ func TestListen(t *testing.T) {
 }
 
 func TestShutdown(t *testing.T) {
+	t.Run("ServerMustShutdown", func(t *testing.T) {
+		ppfs, err := pproftoggle.NewServer(pproftoggle.ServerConfig{
+			HttpServer: &http.Server{Addr: ":" + PORT},
+		})
+		if err != nil {
+			t.Errorf("unable to initialise new server")
+		}
 
+		go func() {
+			err := ppfs.Listen(context.Background())
+			if err != nil && err != http.ErrServerClosed {
+				t.Errorf("errors while listening err:[%s]", err.Error())
+			}
+		}()
+
+		for i := 0; i < 5; i++ {
+			if isPortUsed(PORT) {
+				break
+			}
+			time.Sleep(time.Millisecond * 10)
+		}
+		if !isPortUsed(PORT) {
+			t.Errorf("port:[%s] not being used", PORT)
+		}
+
+		resp := ""
+		if err := requests.URL(ENDDPOINT).BaseURL(BASE_URL).ToString(&resp).Fetch(context.Background()); err != nil {
+			t.Errorf("unable to fetch from endpoint err:[%s]", err)
+		} else {
+			t.Logf("received a respose from the endpoint truncated resp\n[%s]", resp[0:30])
+		}
+
+		ppfs.Shutdown()
+
+		if err := requests.URL(ENDDPOINT).BaseURL(BASE_URL).ToString(&resp).Fetch(context.Background()); err == nil {
+			t.Errorf("received a respose from the endpoint truncated resp\n[%s]", resp[0:30])
+		} else {
+			t.Logf("as expected unable to fetch from endpoint err:[%s]", err)
+		}
+	})
+}
+
+func TestIsRunning(t *testing.T) {
+	ppfs, err := pproftoggle.NewServer(pproftoggle.ServerConfig{
+		HttpServer: &http.Server{Addr: ":" + PORT},
+	})
+	if err != nil {
+		t.Errorf("unable to initialise new server")
+	}
+
+	go func() {
+		err := ppfs.Listen(context.Background())
+		if err != nil && err != http.ErrServerClosed {
+			t.Errorf("errors while listening err:[%s]", err.Error())
+		}
+	}()
+
+	for i := 0; i < 5; i++ {
+		if isPortUsed(PORT) {
+			break
+		}
+		time.Sleep(time.Millisecond * 10)
+	}
+	if !isPortUsed(PORT) {
+		t.Errorf("port:[%s] not being used", PORT)
+	}
+
+	resp := ""
+	if err := requests.URL(ENDDPOINT).BaseURL(BASE_URL).ToString(&resp).Fetch(context.Background()); err != nil {
+		t.Errorf("unable to fetch from endpoint err:[%s]", err)
+	} else {
+		t.Logf("received a response from the endpoint truncated resp\n[%s]", resp[0:30])
+		status := ppfs.IsRunning()
+		if !status {
+			t.Errorf("received response but IsRunning is showing [%v]", status)
+		} else {
+			t.Logf("as expected showing server running [%v]", status)
+		}
+	}
+
+	if err := ppfs.Shutdown(); err != nil {
+		t.Errorf("unexpected error while shutting down err:[%s]", err.Error())
+	}
+
+	if err := requests.URL(ENDDPOINT).BaseURL(BASE_URL).ToString(&resp).Fetch(context.Background()); err == nil {
+		t.Errorf("received response from enpoint even after shutting down resp\n[%s]", resp[0:10])
+	}
+
+	if isPortUsed(PORT) {
+		t.Errorf("port [%s] is still being used after shuttiing down", PORT)
+	}
+}
+
+func isPortUsed(port string) bool {
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(LOCALHOST, port), time.Millisecond*10)
+	if err != nil {
+		return false
+	}
+
+	if conn != nil {
+		defer conn.Close()
+		return true
+	}
+
+	return false
 }
