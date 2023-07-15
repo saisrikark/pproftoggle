1ba2eec368dc0fecb2499bf6dd1a4975f903d494
saisrikark <saisrikark@gmail.com>
2023-07-15 11:27:04 +0530

    updating untracked iles

---
+++ rules/yaml.go
@@ -0,0 +1,43 @@
+package rules
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v3"
+)
+
+type SimpleYamlRule struct {
+	Key   string
+	Value string
+	Path  string
+}
+
+func (yr SimpleYamlRule) Name() string {
+	return fmt.Sprintf(
+		"yaml rule based on key:%s value:%s", yr.Key, yr.Value)
+}
+
+func (yr SimpleYamlRule) Matches() (bool, error) {
+	data, err := ioutil.ReadFile(yr.Path)
+	if err != nil {
+		return false, err
+	}
+
+	// assuming the key value pair will be present at the root
+	var yamlData map[string]interface{}
+	if err := yaml.Unmarshal(data, &yamlData); err != nil {
+		return false, err
+	}
+
+	if val, ok := yamlData[yr.Key]; !ok {
+		return false, nil
+	} else {
+		cval, ok := val.(string)
+		if !ok {
+			return false, nil
+		} else {
+			return yr.Value == cval, nil
+		}
+	}
+}
--- toggler_test.go
+++ toggler_test.go
@@ -11,43 +11,4 @@ import (
 )
 
 func TestServe(t *testing.T) {
-	_, err := pproftoggle.NewToggler(
-		pproftoggle.Config{
-			PollInterval: time.Second * 3,
-			HttpServer: &http.Server{
-				Addr: ":8080",
-			},
-			Rules: []pproftoggle.Rule{
-				rules.EnvVarRule{
-					Key:   "abcd",
-					Value: "efgh",
-				},
-			},
-		})
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// set := true
-	// go func() {
-	// 	for {
-	// 		time.Sleep(time.Second * 2)
-	// 		if set {
-	// 			fmt.Println("setting")
-	// 			os.Setenv("abcd", "efgh")
-	// 			set = false
-	// 		} else {
-	// 			fmt.Println("unsetting")
-	// 			os.Unsetenv("abcd")
-	// 			set = true
-	// 		}
-	// 	}
-	// }()
-
-	// ctx, cancel := context.WithTimeout(context.Background(), time.Second*1000)
-	// defer cancel()
-
-	// if err := toggler.Serve(ctx); err != nil {
-	// 	fmt.Println(err)
-	// }
 }
